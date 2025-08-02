817db8d389b4a840142af93a83960695a860250e
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2025-08-02 11:54:32 -0400

    Update build_all.ps1

--- scripts/build_all.ps1
+++ scripts/build_all.ps1
@@ -1,12 +1,9 @@
 & "$PSScriptRoot\..\gitmodules\mod-installer\build_all.ps1" `
-  -Version "<version>" `
-  -PreReleaseVersion "beta.1" `
-  -OutputFilePrefix "<output file prefix>" `
-  -ModName "<mod name>" `
-  -ModFolder "<mod folder>" `
-  -AdditionalModFolders @() `
+  -Version "1.0.0" `
+  -OutputFilePrefix "Joutomaa" `
+  -ModName "Joutomaa" `
+  -ModFolder "Joutomaa" `
   -ModFilesPath "$PSScriptRoot\..\game" `
-  -ModBaseFilesUrl "<url to mod base files zip>" `
-  -ModBaseFilesUrlHash "<mod base files zip sha256>" `
-  -ModReadmePath "<mod readme path>" `
+  -ModBaseFilesUrl "https://github.com/HL2-Mods-Legacy-Project/hl2-legacy-registry/releases/download/joutomaa/Joutomaa-Original-Cleaned.zip" `
+  -ModBaseFilesUrlHash "5ec834fd48b431e96e191c019337770981b03826943519332182a52aa156ec92" `
   -PublishPath "$PSScriptRoot\..\artifacts\publish"
